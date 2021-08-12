51b334f71e1f61dc51868911f4f6b26070b823cd
ruru112233 <esxwaz1234@gmail.com>
2021-08-12 15:39:58 +0900

    旋回機能の追加

---
+++ Assets/Scripts/Item/AttackPlas.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackPlas : Item
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+
+    }
+}
---
+++ Assets/Scripts/Item/AttackUp.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackUp : Item
+{
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void OnTriggerEnter(Collider other)
+    {
+        base.OnTriggerEnter(other);
+
+        player.At += 5;
+    }
+}
